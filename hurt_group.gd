81a763cd8ed886ee6bc80be5a8c2b37e53a3d747
hlalljie <hlalljie@gmail.com>
2024-09-26 17:40:08 -0700

    fixed hurtgroup boxes by refactoring out single polygon optimization

--- hurt_group.gd
+++ hurt_group.gd
@@ -1,11 +1,11 @@
 extends TileMapLayer
 
 @onready var parent_layer: TileMapLayer = get_parent()
-@onready var hurt_collision_polygon: CollisionPolygon2D = $HurtPolygon/CollisionPolygon2D
-@onready var hurt_polygon_timer: Timer = $HurtPolygon/DisableTimer
 @onready var static_collision_body: StaticBody2D = $StaticBody2D
-@onready var static_collision_polygon: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
+@onready var hurt_area: Area2D = $HurtArea
+@onready var hurt_disable_timer: Timer = $HurtDisableTimer
 @onready var cell_coordinates: Array[Vector2i] = get_used_cells()
+
 
 @export var hp = 100
 
@@ -13,23 +13,22 @@ func _ready():
 	create_combined_collision_polygon()
 	# turn off collisions on parent
 	parent_layer.collision_enabled = false
-	visible = false
+
 
 func create_combined_collision_polygon() -> void:
-	# create an empty polygon to merge with all parent polygons
-	var new_collision_polygon_points: PackedVector2Array = PackedVector2Array()
-
+	var cell_polygons = []
 	for cell in cell_coordinates:
-		
-		# apply coordinates to parent tile data's collision polygon
-		var parent_poly: PackedVector2Array = get_cell_collision_polygon(cell)
-
-		# merge the transformed polygon with the new collision polygon
-		new_collision_polygon_points = Geometry2D.merge_polygons(new_collision_polygon_points, parent_poly)[0]
-
-	# set collision polygons for hurtgroup to our new combined collision polygon
-	hurt_collision_polygon.set_polygon(new_collision_polygon_points)
-	static_collision_polygon.set_polygon(new_collision_polygon_points)
+		# get cell's collision polygon points
+		var new_polygon_points: PackedVector2Array = get_cell_collision_polygon(cell)
+		# create new collision and hurt polygons
+		var new_collision_polygon = CollisionPolygon2D.new()
+		var new_hurt_polygon = CollisionPolygon2D.new()
+		# set the hurt and collision polygon's polygon
+		new_collision_polygon.polygon = new_polygon_points
+		new_hurt_polygon.polygon = new_polygon_points
+		# add collision polygon as a child of the static body and hurt polygon as a child of the hurt area
+		static_collision_body.add_child(new_collision_polygon)
+		hurt_area.add_child(new_hurt_polygon)
 
 
 func get_cell_collision_polygon(cell: Vector2i) -> PackedVector2Array:
@@ -48,28 +47,50 @@ func get_cell_collision_polygon(cell: Vector2i) -> PackedVector2Array:
 	for point in local_poly:
 		translated_polygon.append(Vector2(Vector2i(point) + coordinate_adjustment))
 	
-	# remove parent tile data's collision polygon
-	# tile_data.remove_collision_polygon(0, 0)
-	
 	return translated_polygon
 
 
-func _on_hurt_polygon_hurt(damage: int, _angle: Vector2, _knockback: int) -> void:
+func _on_hurt_area_area_entered(area: Area2D) -> void:
+	# checks attacks
+	if area.is_in_group("attack"):
+		# if the attack does damage
+		if not area.get("damage") == null:
+			# disables the hurtbox of the victim
+			hurt_area.call_deferred("set", "disabled", true)
+			# activates the cooldown timer, 
+			# which will reactive the hurtbox once done
+			hurt_disable_timer.start()
+			# collect hurt infomormation from damaging party
+			take_damage(area.damage)
+
+# Stop disabling hurtbox on timeout
+func _on_hurt_disable_timer_timeout() -> void:
+	hurt_area.call_deferred("set", "disabled", false)
+	
+func take_damage(damage: int) -> void:
 	hp -= damage
-	#print("Hurt Group hit for %d damage. HP left: %d" % [damage, hp])
+	print("Hurt Group hit for %d damage. HP left: %d" % [damage, hp])
 	if hp <= 0:
 		destroy_group()
 
 func destroy_group() -> void:
 	print("Hurt Group destroyed")
 
-	# Turn off collisions
-	# stop disable timer on hurtbox to prevent it from re-enabling the hurt polygon
-	hurt_polygon_timer.stop()
-	# disable hurt timer
-	hurt_collision_polygon.set_deferred("disabled", true)
+	## Turn off collisions
+
+	# stop disable timer on hurt area to prevent it from re-enabling the hurt polygon
+	hurt_disable_timer.stop()
+	# disable all collision polygons in hur area
+	for child in hurt_area.get_children():
+		child.set_deferred("disabled", true)
+	# disable hurtarea
+	hurt_area.set_deferred("disabled", true)
+	# disable all collision polygons in collision body
+	for child in static_collision_body.get_children():
+		child.set_deferred("disabled", true)
 	# disable collision body
-	static_collision_polygon.set_deferred("disabled", true)
+	static_collision_body.set_deferred("disabled", true)
+
 	
 	# switch the sprite for every item in the group and show below everything but the floor
 	for cell in cell_coordinates:
